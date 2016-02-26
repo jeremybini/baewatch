1d7c1d44af239619549f843bde65ba54329ebcb3
sp6pe <sp6pe@virginia.edu>
2016-02-26 17:01:37 -0500

    stuff

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("app", ["firebase","ui.router"]);
 
 app.config(function($urlRouterProvider, $locationProvider, $httpProvider, $stateProvider) {
   // remove '#' from urls
-  $locationProvider.html5Mode(true);
+  // $locationProvider.html5Mode(true);
   // invalid routes redirect to the root
   $urlRouterProvider.otherwise('/');
 
@@ -38,13 +38,32 @@ app.controller("main", function($scope,Profile,$state) {
 });
 
 app.controller("profileCtrl", function($scope, data) {
-  data.socialProfiles.forEach(function(profile) {
-    var photo = data.photos.filter(function(p) {
-      return p.typeName === profile.typeName
-    })[0]
-    if(photo) profile.photoUrl = photo.url
-    else profile.photoUrl = "img/portfolio/cake.png";
-  });
+  // data.socialProfiles.forEach(function(profile) {
+  //   var photo = data.photos.filter(function(p) {
+  //     return p.typeName === profile.typeName
+  //   })[0]
+  //   if(photo) profile.photoUrl = photo.url
+  //   else profile.photoUrl = "img/portfolio/cake.png";
+  // });
+
+data.socialProfiles.forEach(function(profile){
+  if(profile.typeName === 'Facebook') profile.photoUrl = "img/portfolio/facebook.png";
+  else if(profile.typeName === 'Twitter') profile.photoUrl = "img/portfolio/twitter.png";
+  else if(profile.typeName === 'Klout') profile.photoUrl = "img/portfolio/klout.png";
+  else if(profile.typeName === 'Github') profile.photoUrl = "img/portfolio/github.png";
+  else if(profile.typeName === 'AngelList') profile.photoUrl = "img/portfolio/angelist.png";
+  else if(profile.typeName === 'LinkedIn') profile.photoUrl = "img/portfolio/linkedin.png";
+  else if(profile.typeName === 'GooglePlus') profile.photoUrl = "img/portfolio/googleplus.png";
+  else if(profile.typeName === 'Pinterest') profile.photoUrl = "img/portfolio/pinterest.png";
+  else if(profile.typeName === 'Foursquare') profile.photoUrl = "img/portfolio/foursquare.png";
+  else if(profile.typeName === 'Vimeo') profile.photoUrl = "img/portfolio/viemo.png";
+  else if(profile.typeName === 'Myspace') profile.photoUrl = "img/portfolio/myspace.png";
+  else if(profile.typeName === 'Flickr') profile.photoUrl = "img/portfolio/flickr.png";
+  else if(profile.typeName === 'Gravatar') profile.photoUrl = "img/portfolio/gravatar.png";
+  else profile.photoUrl = "img/portfolio/submarine.png"
+
+
+})
 
   console.log(data)
   $scope.data = data
