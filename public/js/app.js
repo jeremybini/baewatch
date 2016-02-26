d9119293bfc06c2c0d09f64eea0b811f23ae77cb
elpenao <elpenao@gmail.com>
2016-02-26 14:01:48 -0500

    uirouter

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,39 @@
-var app = angular.module("app", ["firebase"]);
+var app = angular.module("app", ["firebase","ui.router"]);
 
-app.controller("main", function($scope, $firebaseObject, $http) {
-  var ref = new Firebase("https://baespy.firebaseio.com/data");
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+app.config(function($urlRouterProvider, $locationProvider, $httpProvider, $stateProvider) {
+  // remove '#' from urls
+  $locationProvider.html5Mode(true);
+  // invalid routes redirect to the root
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('main', {
+    url: "/" ,
+    templateUrl:"../templates/search.template.html",
+    controller: 'main'
+  })
+
+  $stateProvider.state('profile', {
+    url: "/profile" ,
+    templateUrl: "../templates/profile.template.html",
+    controller: 'profileCtrl'
+  })
+
+})
+
+app.controller("main", function($scope) {
 
   $scope.search = function(email){
   	//make request using to api
-
+    // $state.go('profile',{email:email})
   	//get info
 
   	//add to db 
   }
 
 });
+
+app.controller("profileCtrl", function($scope) {
+
+});
+
 
